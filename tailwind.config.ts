e1152955f2043436894dcefe478287601ec684f6
coresyt <corejsyt2011@gmail.com>
2024-12-18 18:09:38 -0600

    feat(Root): Create base project skeleton

---
+++ tailwind.config.ts
@@ -0,0 +1,9 @@
+import type { Config } from 'tailwindcss'
+
+export default {
+  content: ['./src/App.tsx', './src/components/**/*.tsx'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+} satisfies Config
